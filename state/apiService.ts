82b96e7696ff16f35cd5e24d3bb8e542011c5d51
Ashwin Pandey <ap.ashwinpandey@gmail.com>
2024-10-23 00:24:40 -0400

    Add apiService module for fetching dashboard metrics from the backend server

---
+++ state/apiService.ts
@@ -0,0 +1,69 @@
+import axios from "axios";
+
+export interface Product {
+  productId: string;
+  name: string;
+  price: number;
+  rating?: number;
+  stockQuantity: number;
+}
+
+export interface NewProduct {
+  name: string;
+  price: number;
+  rating?: number;
+  stockQuantity: number;
+}
+
+export interface SalesSummary {
+  salesSummaryId: string;
+  totalValue: number;
+  changePercentage?: number;
+  date: string;
+}
+
+export interface PurchaseSummary {
+  purchaseSummaryId: string;
+  totalPurchased: number;
+  changePercentage?: number;
+  date: string;
+}
+
+export interface ExpenseSummary {
+  expenseSummarId: string;
+  totalExpenses: number;
+  date: string;
+}
+
+export interface ExpenseByCategorySummary {
+  expenseByCategorySummaryId: string;
+  category: string;
+  amount: string;
+  date: string;
+}
+
+export interface DashboardMetrics {
+  popularProducts: Product[];
+  salesSummary: SalesSummary[];
+  purchaseSummary: PurchaseSummary[];
+  expenseSummary: ExpenseSummary[];
+  expenseByCategorySummary: ExpenseByCategorySummary[];
+}
+
+export interface User {
+  userId: string;
+  name: string;
+  email: string;
+}
+
+
+export const fetchDashboardMetrics = async () => {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/dashboard`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch dashboard metrics:", error);
+  }
+}

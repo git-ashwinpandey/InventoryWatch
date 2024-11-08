a250cfac5a9a336fbf3ec3ecd6aa581232522e16
Ashwin Pandey <ap.ashwinpandey@gmail.com>
2024-11-08 13:45:00 -0500

    Add random image rendering to CardPopularProducts component using Picsum

--- app/dashboard/CardPopularProducts.tsx
+++ app/dashboard/CardPopularProducts.tsx
@@ -3,6 +3,7 @@ import { DashboardMetrics, fetchDashboardMetrics } from '@/state/apiService'
 import { BackpackIcon } from '@radix-ui/react-icons';
 import React, { useEffect, useState } from 'react'
 import Ratings from '../(components)/Ratings/ratings';
+import Image from "next/image";
 
 const CardPopularProducts = () => {
     const [data, setData] = useState<DashboardMetrics | null>(null);
@@ -40,7 +41,13 @@ const CardPopularProducts = () => {
                         className='flex items-center justify-between gap-3 px-5 py-7 border-b dark:border-slate-700'
                     >
                         <div className='flex items-center gap-3'>
-                            <div>img</div>
+                            <Image
+                                src={`https://picsum.photos/200/300?random=${Math.floor(Math.random() * 3) + 1}`}
+                                alt={product.name}
+                                width={35}
+                                height={35}
+                                className="mb-3 rounded-2xl w-15 h-13"
+                            />
                             <div className='flex flex-col justify-between gap-1'>
                                 <div className='font-bold text-gray-700 dark:text-white'>{product.name}</div>
                                 <div className='flex text-sm items-center'>
@@ -48,14 +55,14 @@ const CardPopularProducts = () => {
                                         {product.price}
                                     </span>
                                     <span className='mx-2'>|</span>
-                                    <Ratings rating={product.rating || 0}/>
+                                    <Ratings rating={product.rating || 0} />
                                 </div>
                             </div>
                         </div>
 
                         <div className='text-xs flex items-center'>
                             <button className='p-2 rounded-full bg-blue-100 text-blue-600 mr-2'>
-                                <BackpackIcon className='w-4 h-4'/>
+                                <BackpackIcon className='w-4 h-4' />
                             </button>
                             {Math.round(product.stockQuantity / 10000)}k Sold
                         </div>
